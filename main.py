e6e711f8a29ef38c5974792509264ad12d991bf7
LJNERO8 <ljnero8@gmail.com>
2022-10-27 22:54:31 +0300

    Task 1.

--- main.py
+++ main.py
@@ -19,7 +19,34 @@
 print("Task 1. \n")
 
 
+class FibonacciNumbers:
+    n1 = 1
+    n2 = 1
+    iteration = 0
+    max_iterations = 0
 
+    def __init__(self, max_iterations):
+        self.max_iterations = max_iterations
+
+    def __iter__(self):
+        self.n1 = 1
+        self.n2 = 1
+        return self
+
+    def __next__(self):
+        if self.iteration < self.max_iterations:
+            x = self.n1 + self.n2
+            self.n1 = self.n2
+            self.n2 = x
+            self.iteration += 1
+            return x
+        else:
+            raise StopIteration
+
+
+# 1, 1, 2, 3, 5
+for i in FibonacciNumbers(10):
+    print(i)
 #
 # 2.* Implement generator for Fibonacci numbers
 #
